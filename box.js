018d2e0d6c540ad944c94532328b533730f5a23c
VeenaVaikunth <95581824+VeenaVaikunth@users.noreply.github.com>
2023-09-17 12:33:43 -0400

    Add files via upload

---
+++ box.js
@@ -0,0 +1,14 @@
+// Registering component in log-component.js
+AFRAME.registerComponent('move-box-on-click', {
+    schema: {
+      moveX: {type: 'number', default: 0},
+    },
+    tick: function(){
+        window.addEventListener('click', (e) => {
+          this.data.moveX = this.data.moveX + 0.05;
+        })
+        var pos = this.el.getAttribute('position');
+        pos.x = pos.x + this.data.moveX;
+        this.el.setAttribute('position', {x: pos.x, y: pos.y, z: pos.z})
+    }
+  });
